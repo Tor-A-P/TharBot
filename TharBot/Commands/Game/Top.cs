9778f9315e67ae0cef7c48448074c1f4fa7ed61f
Tor André Pettersen <tor.andre.pettersen@gmail.com>
2022-07-09 23:38:05 +0200

    Added functionality to top/leaderboard function
    You can now specify which stat you wanna sort by; messages sent, total exp, or TharCoins

--- TharBot/Commands/Game/Top.cs
+++ TharBot/Commands/Game/Top.cs
@@ -21,38 +21,78 @@ namespace TharBot.Commands
 
         [Command("Top")]
         [Alias("Leaderboard")]
-        [Summary("Shows the top posters in the server by EXP")]
+        [Summary("Shows the top posters in the server, sorted by a choosable statistic, by default number of messages the bot has seen from users.\n" +
+            "**USAGE:** th.top, th.top [SORTING_FLAG]\n" +
+            "**EXAMPLES:** th.top, th.top coins, th.top exp")]
         [Remarks("Game")]
-        public async Task TopAsync()
+        public async Task TopAsync(string flag = "messages")
         {
-            var ServerProfiles = db.LoadRecords<GameServerProfile>("GameProfiles");
-            if (ServerProfiles == null)
+            var serverProfile = db.LoadRecordById<GameServerProfile>("GameProfiles", Context.Guild.Id);
+            if (serverProfile == null)
             {
                 var noServerProfEmbed = await EmbedHandler.CreateUserErrorEmbed("Could not find server profile", "It seems this server has no profile, try sending a message (not a command) and then use this command again!");
                 await ReplyAsync(embed: noServerProfEmbed);
                 return;
             }
+
+            var embed = flag.ToLower() switch
+            {
+                "messages" or "msg" or "msgs" => await EmbedCreator("msg", serverProfile),
+                "exp" or "xp" or "experience" or "level" or "lvl" => await EmbedCreator("exp", serverProfile),
+                "coins" or "money" or "tharcoins" => await EmbedCreator("coins", serverProfile),
+                _ => await EmbedHandler.CreateUserErrorEmbed("Invalid sorting flag", $"\"{flag}\" is not a valid statistic to sort by, " +
+                                        $"valid flags include: messages, exp, coins. If you use the command without a flag at all, it will default to messages."),
+            };
+            await ReplyAsync(embed: embed);
+        }
+
+        public async Task<Embed> EmbedCreator(string flag, GameServerProfile profile)
+        {
             var embed = await EmbedHandler.CreateBasicEmbedBuilder("Leaderboard for this server");
             var nameFieldString = "";
-            var expFieldString = "";
-            foreach (var profile in ServerProfiles)
+            var valueFieldString = "";
+            var leaderBoardPos = 1;
+            if (flag == "msg")
             {
-                if (profile.ServerId == Context.Guild.Id)
+                var sortedUserProfiles = profile.Users.OrderByDescending(x => x.NumMessages);
+                foreach (var userProfile in sortedUserProfiles)
                 {
-                    var sortedProfiles = profile.Users.OrderByDescending(x => x.TotalExp());
-                    var leaderBoardPos = 1;
-                    foreach (var userProfile in sortedProfiles)
-                    {
-                        var user = await _client.GetUserAsync(userProfile.UserId);
-                        nameFieldString += $"{leaderBoardPos}. {user.Mention}\n";
-                        expFieldString += $"{userProfile.TotalExp()}\n";
-                        leaderBoardPos++;
-                    }
+                    var user = await _client.GetUserAsync(userProfile.UserId);
+                    nameFieldString += $"{leaderBoardPos}. {user.Mention}\n";
+                    valueFieldString += $"{userProfile.NumMessages}\n";
+                    leaderBoardPos++;
                 }
+                embed.AddField("Username", nameFieldString, true)
+                     .AddField("Total Messages", valueFieldString, true);
             }
-            embed.AddField("Username", nameFieldString, true)
-                 .AddField("Total EXP", expFieldString, true);
-            await ReplyAsync(embed: embed.Build());
+            else if (flag == "coins")
+            {
+                var sortedUserProfiles = profile.Users.OrderByDescending(x => x.TharCoins);
+                foreach (var userProfile in sortedUserProfiles)
+                {
+                    var user = await _client.GetUserAsync(userProfile.UserId);
+                    nameFieldString += $"{leaderBoardPos}. {user.Mention}\n";
+                    valueFieldString += $"{userProfile.TharCoins}\n";
+                    leaderBoardPos++;
+                }
+                embed.AddField("Username", nameFieldString, true)
+                     .AddField("Total TharCoins", valueFieldString, true);
+            }
+            else
+            {
+                var sortedUserProfiles = profile.Users.OrderByDescending(x => x.TotalExp());
+                foreach (var userProfile in sortedUserProfiles)
+                {
+                    var user = await _client.GetUserAsync(userProfile.UserId);
+                    nameFieldString += $"{leaderBoardPos}. {user.Mention}\n";
+                    valueFieldString += $"{userProfile.TotalExp()}\n";
+                    leaderBoardPos++;
+                }
+                embed.AddField("Username", nameFieldString, true)
+                     .AddField("Total EXP", valueFieldString, true);
+            }
+
+            return embed.Build();
         }
     }
 }
