cb9168d58426a603bd069db5b5c47e98ddfd14be
Tor André Pettersen <tor.andre.pettersen@gmail.com>
2022-09-22 00:47:45 +0200

    Avatar fix
    Avatar function was only grabbing the global avatar, regardless of if the user mentioned had a local one set. Changed so it now grabs local by default, with an optional modifier to grab the global one

--- TharBot/Commands/Info/Avatar.cs
+++ TharBot/Commands/Info/Avatar.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using TharBot.Handlers;
 
@@ -6,23 +7,50 @@ namespace TharBot.Commands
 {
     public class Avatar : ModuleBase<SocketCommandContext>
     {
+        private readonly DiscordSocketClient _client;
+
+        public Avatar(DiscordSocketClient client)
+        {
+            _client = client;
+        }
+
         [Command("Avatar")]
         [Alias("Pic", "pfp")]
-        [Summary("Returns the high-quality version avatar of the user given. If no user specified, returns command user's avatar.\n" +
-                "**USAGE:** th.avatar, th.avatar [@USER_MENTION]")]
+        [Summary("Returns the high-quality version avatar of the user given, by default the local server avatar, optionally the global avatar. If no user specified, returns command user's avatar.\n" +
+                "**USAGE:** th.avatar, th.avatar [@USER_MENTION], th.avatar [@USER_MENTION] global")]
         [Remarks("Info")]
-        public async Task GetUserAvatarAsync(SocketUser? user = null)
+        public async Task GetUserAvatarAsync(SocketUser? user = null, string mode = "local")
         {
             try
             {
                 if (user == null) user = Context.User;
 
-                var embedBuilder = await EmbedHandler.CreateBasicEmbedBuilder($"Avatar for {user.Username}#{user.Discriminator}");
+                var guildUser = Context.Guild.GetUser(user.Id);
+                
+                if (mode.ToLower() == "local")
+                {
+                    var embedBuilder = await EmbedHandler.CreateBasicEmbedBuilder($"Avatar for {guildUser.Username}#{guildUser.Discriminator}");
 
-                var embed = embedBuilder.WithImageUrl(user.GetAvatarUrl(Discord.ImageFormat.Auto, 2048) ?? user.GetDefaultAvatarUrl())
-                    .Build();
+                    var embed = embedBuilder.WithImageUrl(guildUser.GetGuildAvatarUrl(Discord.ImageFormat.Auto, 2048) ?? guildUser.GetAvatarUrl(Discord.ImageFormat.Auto, 2048) ?? guildUser.GetDefaultAvatarUrl())
+                        .Build();
 
-                await ReplyAsync(embed: embed);
+                    await ReplyAsync(embed: embed);
+                }
+                else if (mode.ToLower() == "global")
+                {
+                    var embedBuilder = await EmbedHandler.CreateBasicEmbedBuilder($"Global avatar for {guildUser.Username}#{guildUser.Discriminator}");
+
+                    var embed = embedBuilder.WithImageUrl(guildUser.GetAvatarUrl(Discord.ImageFormat.Auto, 2048) ?? guildUser.GetDefaultAvatarUrl())
+                        .Build();
+
+                    await ReplyAsync(embed: embed);
+                }
+                else
+                {
+                    var wrongModeEmbed = await EmbedHandler.CreateUserErrorEmbed("Avatar", "Please use either \"local\", \"global\" or no modifier!");
+
+                    await ReplyAsync(embed: wrongModeEmbed);
+                } 
             }
             catch (Exception ex)
             {
